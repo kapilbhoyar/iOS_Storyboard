2474963b2c818500b0aeb1af19b881f0053006d3
kapilbhoyar <kapilbhoyarkkb@gmail.com>
2023-10-29 23:37:43 +0530

    Calendar app added

---
+++ Calendar/Calendar/CalendarCell.swift
@@ -0,0 +1,14 @@
+//
+//  CalendarCell.swift
+//  Calendar
+//
+//  Created by Shadow  on 29/10/23.
+//
+
+import UIKit
+
+class CalendarCell: UICollectionViewCell
+{
+    @IBOutlet weak var dayOfMonth: UILabel!
+    
+}

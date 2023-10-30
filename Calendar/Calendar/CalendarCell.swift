6f25febde13bb8e67574b5ab632790079eb8f34f
kapilbhoyar <kapilbhoyarkkb@gmail.com>
2023-10-30 23:40:15 +0530

    error fixed

--- Calendar/Calendar/CalendarCell.swift
+++ Calendar/Calendar/CalendarCell.swift
@@ -1,14 +1,8 @@
-//
-//  CalendarCell.swift
-//  Calendar
-//
-//  Created by Shadow  on 29/10/23.
-//
+
 
 import UIKit
 
 class CalendarCell: UICollectionViewCell
 {
-    @IBOutlet weak var dayOfMonth: UILabel!
-    
+	@IBOutlet weak var dayOfMonth: UILabel!
 }

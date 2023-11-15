ec1acb45b357aeef2d26aa036e938dfbe33be6e8
kapilbhoyar <kapilbhoyarkkb@gmail.com>
2023-11-15 12:18:28 +0530

    Learned about Alerts

---
+++ 06. AlertProject/AlertProject/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  AlertProject
+//
+//  Created by Shadow  on 14/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var password2Text: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func signupClicked(_ sender: Any) {
+        
+//        let alert = UIAlertController(title: "Error!", message: "Username not found", preferredStyle: UIAlertController.Style.alert)
+        
+//        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) { UIAlertAction in
+//            //button clicked
+//            print("button clicked")
+//        }
+//        alert.addAction(okButton)
+        
+        
+        if(usernameText.text == "") {
+            makeAlert(titleInput: "Error", messageInput: "Username not found!")
+        }else if(passwordText.text == "") {
+            makeAlert(titleInput: "Error", messageInput: "Password not found!")
+        }else if(passwordText.text != password2Text.text) {
+            makeAlert(titleInput: "Error", messageInput: "Password do not match!")
+        }else {
+            makeAlert(titleInput: "Success", messageInput: "User signed up successfully!")
+        }
+        
+        func makeAlert(titleInput:String, messageInput:String) {
+            let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+            let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+            alert.addAction(okButton)
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+//        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
+}
+
